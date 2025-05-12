e19a8addf2d02953a2536281936cffaba862cf03
nathannavmoondi <nav.moondi@gmail.com>
2025-05-12 10:07:05 -0400

    fix: update title and enhance NavBar with GitHub button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image, Button } from '@chakra-ui/react'
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
@@ -12,8 +12,14 @@ const NavBar = ({ onSearch }: Props) => {
     <HStack padding='10px'>      
       {/* <Image src='https://static.vecteezy.com/system/resources/previews/017/744/677/non_2x/video-game-icon-png.png' boxSize={'60px' as string} /> */}
 
-      <span>Game Hub - Nathan Moondi</span>
+      <span><b>Game Hub - Nathan Nav Moondi</b></span>
       <SearchInput onSearch={onSearch} />
+      <Button 
+        colorScheme='blue' 
+        onClick={() => window.open('https://github.com/nathannavmoondi/game-hub', '_blank')}
+      >
+        Github
+      </Button>
       <ColorModeSwitch />
     </HStack>
   )
